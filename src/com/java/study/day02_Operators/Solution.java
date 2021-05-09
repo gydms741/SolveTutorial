a9aab500de8748c2cfdf2f90234e4d104f5cd95d
gydms741 <gydms741@naver.com>
2021-05-09 14:35:30 +0900

    add the description to day02

--- src/com/java/study/day02_Operators/Solution.java
+++ src/com/java/study/day02_Operators/Solution.java
@@ -1,5 +1,37 @@
 package com.java.study.day02_Operators;
+/*
+    **Task**
+    Given the meal price(base cost of a meal),tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the *meal price being added as tax) for a meal, find and print the meal's total cost. Round the result to the nearest integer.
 
+    **Example**
+    * mealCost = 100
+    * tipPercent = 15
+    * taxPercent = 8
+    A tip of 15% * 100 = 15, and the taxes are 8% * 100 = 8. Print the value 123 and return from the function.
+
+    **Function Description**
+    Complete the solve function in the editor below.
+    solve has the following parameters:
+    - int meal_cost: the cost of food before tip and tax
+    - int tip_percent: the tip percentage
+    - int tax_percent: the tax percentage
+    Returns The function returns nothing. Print the calculated value, rounded to the nearest integer.
+
+    **Note:** Be sure to use precise values for your calculations, or you may end up with an incorrectly rounded result.
+
+    **Input Format**
+    There are 3 lines of numeric input:
+    The first line has a double, mealCost(the cost of the meal before tax and tip).
+    The second line has an integer, tipPercent(the percentage of mealCost being added as tip).
+    The third line has an integer,taxPercent(the percentage of mealCost being added as tax).
+
+    **Sample Input**
+    `12.00
+    20
+    8`
+    **Sample Output**
+    `15`
+ */
 import java.io.*;
 
 class Result {
