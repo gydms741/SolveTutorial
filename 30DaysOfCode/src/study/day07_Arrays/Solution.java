427e3e56b4fa109d0145e01a7196fbadc24b9f7b
gydms741 <gydms741@naver.com>
2021-05-23 12:58:49 +0900

    couldn't day07's problem solving yet

--- 30DaysOfCode/src/study/day07_Arrays/Solution.java
+++ 30DaysOfCode/src/study/day07_Arrays/Solution.java
@@ -7,21 +7,30 @@ import java.util.stream.Stream;
 
 public class Solution {
     public static void main(String[] args) throws IOException{
+
+
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         //The size of our array
         int n = Integer.parseInt(bufferedReader.readLine().trim());
 
+        int out = n;
+
         List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$","").split(" "))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
 
-        for (int i = n-1; i>=0; i--){
+        //sample input
+        //4
+        //1 4 3 2
+        for (int i = n-1; i>=0; i--) {
+            //out += arr.add(i);
             //System.out.println(arr[i]+"");
-            System.out.println(arr);
-            arr[i] = arr;
-        }
+            //String[] temp = arr;
+            //System.out.println(arr[i]+"");
+            //arr[i] = arr;
 
-        bufferedReader.close();
+            bufferedReader.close();
+        }
     }
 }
