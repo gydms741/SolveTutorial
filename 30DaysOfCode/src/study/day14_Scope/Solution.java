a5e52fa2fadbdf1d3c6deb7c00585300aa4bc6b4
gydms741 <gydms741@naver.com>
2021-06-10 14:44:46 +0900

    solved day14's challenge

---
+++ 30DaysOfCode/src/study/day14_Scope/Solution.java
@@ -0,0 +1,43 @@
+package study.day14_Scope;
+
+import java.util.Scanner;
+
+class Difference{
+        private int[] elements;
+        public int maximumDifference;
+
+    public Difference(int[] a) {
+        this.elements = a;
+
+    }
+
+    public void computeDifference(){
+        int min = elements[0];
+        int max = elements[0];
+        for (int i = 0; i<elements.length;i++){
+            if (elements[i]<min){
+                min=elements[i];
+            }if (elements[i]>max){
+                max=elements[i];
+            }
+        }
+        maximumDifference = Math.abs(max-min);
+        }
+}
+
+public class Solution {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++){
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+        difference.computeDifference();
+        System.out.println(difference.maximumDifference);
+    }
+}
